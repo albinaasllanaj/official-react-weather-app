40de3e472586f8425c0f1d9c1597dd1ccab372de
Albina Asllanaj <112036955+albinaasllanaj@users.noreply.github.com>
2022-11-30 15:51:21 -0500

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-    Official Weather App  
-    </div>
+    <h1>Weather App</h1> 
+    <footer>
+This project was created by <a href="https://fascinating-lamington-980873.netlify.app/index.html" rel="noreferrer" target="_blank">Albina Asllanaj </a> and is {""}    
+    <a href="https://github.com/albinaasllanaj/official-react-weather-app" rel="noreferrer" target="_blank">open-sourced on GitHub.</a>
+     </footer>
+     </div>
   );
 }
 
