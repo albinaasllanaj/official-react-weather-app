eccc8b5f091bbc710ce872cc0202f4a01d9f485e
Albina Asllanaj <112036955+albinaasllanaj@users.noreply.github.com>
2022-12-01 14:25:07 -0500

    Added html of the weather app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
+import Weather from "./Weather"
 import './App.css';
+
 
 export default function App() {
   return (
     <div className="App">
       <div className="container ">
-    <h1>Weather App</h1> 
+    
+    <Weather />
     <footer>
 This project was created by <a href="https://fascinating-lamington-980873.netlify.app/index.html" rel="noreferrer" target="_blank">Albina Asllanaj </a> and is {""}    
     <a href="https://github.com/albinaasllanaj/official-react-weather-app" rel="noreferrer" target="_blank">open-sourced on GitHub.</a>
