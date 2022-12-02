3c9ca552084f2260e943bc17e1c43a455f824f80
Albina Asllanaj <112036955+albinaasllanaj@users.noreply.github.com>
2022-12-02 15:20:35 -0500

    removed city variable

--- src/Weather.js
+++ src/Weather.js
@@ -68,7 +68,6 @@ return (
     );
     } else {
         const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let city = "New York"
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(handleResponse);
     
